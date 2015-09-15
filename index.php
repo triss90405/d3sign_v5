55603108c32f4cbab5934b987883b189f46e7957
triss90405 <tw@d3sign.dk>
2015-09-15 15:01:50 +0200

    Adding work section

--- index.php
+++ index.php
@@ -2,5 +2,6 @@
 
 <?php include("parts/section_header.html"); ?>
 <?php include("parts/section_services.html"); ?>
+<?php include("parts/section_work.html"); ?>
 
 <?php include("parts/footer.html"); ?>
