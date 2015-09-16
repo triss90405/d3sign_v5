73addd215cf73770554a235bafff67de78672abd
triss90405 <tw@d3sign.dk>
2015-09-16 18:50:04 +0200

    Adding contact section

--- index.php
+++ index.php
@@ -4,5 +4,6 @@
 <?php include("parts/section_services.html"); ?>
 <?php include("parts/section_work.html"); ?>
 <?php include("parts/section_clients.html"); ?>
+<?php include("parts/section_contact.html"); ?>
 
 <?php include("parts/footer.html"); ?>
