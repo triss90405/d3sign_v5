04985b19eabdc90271ecc19b33458bac6d712946
triss90405 <tw@d3sign.dk>
2015-09-16 19:03:35 +0200

    Adding content section (videos/articles)

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <?php include("parts/section_header.html"); ?>
 <?php include("parts/section_services.html"); ?>
 <?php include("parts/section_work.html"); ?>
+<?php include("parts/section_content.html"); ?>
 <?php include("parts/section_clients.html"); ?>
 <?php include("parts/section_contact.html"); ?>
 
