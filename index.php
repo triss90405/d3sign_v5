2fdf1590624918f26a185f56211f4c2c4a8b04b1
triss90405 <tw@d3sign.dk>
2015-09-16 16:53:49 +0200

    Adding Clients section

--- index.php
+++ index.php
@@ -3,5 +3,6 @@
 <?php include("parts/section_header.html"); ?>
 <?php include("parts/section_services.html"); ?>
 <?php include("parts/section_work.html"); ?>
+<?php include("parts/section_clients.html"); ?>
 
 <?php include("parts/footer.html"); ?>
